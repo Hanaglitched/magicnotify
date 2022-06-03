4c825c3ecda46c1b59c0ac245c6bdb70b5d132f4
Hanaglitched <neincredits@knou.ac.kr>
2022-06-03 12:23:28 +0900

    Initial commit

---
+++ src/main/java/com/hanaglitched/magicnotify/MagicnotifyApplication.java
@@ -0,0 +1,13 @@
+package com.hanaglitched.magicnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicnotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicnotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanaglitched/magicnotify/MagicnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanaglitched.magicnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicnotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
