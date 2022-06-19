1d31431bdf04004e09b55be07b102c5b95158549
Hanaglitched <neincredits@knou.ac.kr>
2022-06-19 23:24:30 +0900

    json parse revise
    new json parse

--- src/main/java/com/hanaglitched/magicnotify/MagicnotifyApplication.java
+++ src/main/java/com/hanaglitched/magicnotify/MagicnotifyApplication.java
@@ -16,23 +16,31 @@ public class MagicnotifyApplication {
 		try {
 			//https://crunchify.com/how-to-read-json-object-from-file-in-java/
 			//parse object json.
-			Object obj = parser.parse(new FileReader("/my projects/magicnotify/src/main/resources/json/AllPrintings.json"));
+			Object obj = parser.parse(new FileReader("/my projects/magicnotify/src/main/resources/json/output.json"));
 
 			// A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
-			JSONObject jsonObject = (JSONObject) obj;
+			JSONArray jsonObject = (JSONArray) obj;
 
 			// A JSON array. JSONObject supports java.util.List interface.
-			JSONObject data = (JSONObject) jsonObject.get("data"); // magic 2014 core set get
-			JSONObject m14 = (JSONObject) data.get("M14");
-			JSONArray cards = (JSONArray) m14.get("cards"); // cards.
-			JSONObject firstCard = (JSONObject) cards.get(0);
+			JSONObject data = (JSONObject) jsonObject.get(0); // parse 1 : first array
+			JSONObject get_data = (JSONObject) data.get("53532290-9194-5066-9318-b9cc09c52d8f"); // parse 2 : key value parse
+			String name = (String) get_data.get("name");
 
-			System.out.println(firstCard.get("name").toString());
-			JSONObject identifiers = (JSONObject) firstCard.get("identifiers");
-			String multiverseId = identifiers.get("multiverseId").toString();
-			String uuid = firstCard.get("uuid").toString();
+			System.out.println("parsing normal data : ");
+			System.out.println("data");
+
+			System.out.println(data.toString());
+			System.out.println("get_data");
+			System.out.println(get_data.toString());
+			System.out.println("name : " + name);
+
+			String multiverseId = "";
+			if(get_data.get("multiverseId") == null) multiverseId = "null";
+			else multiverseId = get_data.get("multiverseId").toString();
+
+			String uuid = "53532290-9194-5066-9318-b9cc09c52d8f";
 			System.out.println("multiverseId : " + multiverseId);
-			System.out.println("uuid : " + uuid);
+			System.out.println("uuid : " + "53532290-9194-5066-9318-b9cc09c52d8f");
 
 			Object price = parser.parse(new FileReader("/my projects/magicnotify/src/main/resources/json/AllPrices.json"));
 			JSONObject priceObject = (JSONObject) price;
@@ -40,7 +48,7 @@ public class MagicnotifyApplication {
 			int i = 0;
 			for(i = 0; i < data_.size(); i++){
 				JSONObject uuid_array = (JSONObject) data_.get(i);
-				if(uuid.toString() == uuid_array.toString()){
+				if(uuid == uuid_array.toString()){
 					break;
 				}
 			}
